988a77fc3c99f22d40cd5b39f026dd222e703a01
Mac Mini <macmini@Mac-mini.local>
2025-05-24 03:16:08 +1200

    Update redirect_uri to deployed URL

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(cors());
 
 const client_id = '4595aa31219c4fbabf83fe388bf55682'; // Replace if needed
 const client_secret = '69c9de26bf1c48899e5e933f53dd5b90'; // Replace this with your actual Spotify secret
-const redirect_uri = 'http://127.0.0.1:3000/callback';
+const redirect_uri = 'https://spotify-auth-server-dxij.onrender.com/callback';
 
 app.get('/login', (req, res) => {
   const scope = 'user-read-private user-read-email';
